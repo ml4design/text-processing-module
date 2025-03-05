1539949642fe2703238f67ea4e3bf79fc8f77f0d
Carlo van der Valk <cvandervalk@gmail.com>
2025-03-05 12:11:59 +0100

    Update preprocessing.py

--- preprocessing.py
+++ preprocessing.py
@@ -1,8 +1,8 @@
-
 import string
 from nltk.tokenize import word_tokenize
 from nltk.corpus import stopwords
 from nltk import SnowballStemmer
+from nltk.stem import WordNetLemmatizer
 
 
 ######################################
@@ -10,32 +10,43 @@ from nltk import SnowballStemmer
 ######################################
 # Input: Text
 # Output: List with words
-def preprocess(text_data, lower=True, rem_punc=True, word_tokenization=True, rem_numb=True, rem_stopwords=True, extra_stopwords=[], stem=True):
-  extra_stopwords = ["n\'t","’","\'s"]
-  if lower:
-    #lowercase text
-    tokens = text_data.lower()
+def preprocess(text_data,
+               lower=True,
+               rem_punc=True,
+               word_tokenization=True,
+               rem_numb=True,
+               rem_stopwords=True,
+               extra_stopwords=[],
+               stem=True,
+               lem=True):
+    # extra_stopwords = ["n\'t","’","\'s"]
+    tokens = text_data
+    if lower:
+        #lowercase text
+        tokens = text_data.lower()
 
-  if word_tokenization:
-    # tokenize text by words
-    tokens = word_tokenize(text_data)
+    if word_tokenization:
+        # tokenize text by words
+        tokens = word_tokenize(tokens)
 
-  if rem_numb:
-    # # Remove numbers
-    tokens = [w for w in tokens if not w.isdigit()]
+    if rem_numb:
+        # # Remove numbers
+        tokens = [w for w in tokens if not w.isdigit()]
 
-  if rem_punc:
-    # # remove punctuation
-    tokens = [token for token in tokens if not token in string.punctuation]
-  if rem_stopwords:
-    # # remove stopwords
-    lang_stopwords = stopwords.words("english")
-    lang_stopwords+=extra_stopwords
-    tokens = [token for token in tokens if token not in lang_stopwords]
-  if stem:
-    # # stemming 
-    lang="english"
-    stemmer = SnowballStemmer(lang) 
-    tokens = [stemmer.stem(token) for token in tokens]
-  return tokens
-
+    if rem_punc:
+        # # remove punctuation
+        tokens = [token for token in tokens if not token in string.punctuation]
+    if rem_stopwords:
+        # # remove stopwords
+        lang_stopwords = stopwords.words("english")
+        lang_stopwords += extra_stopwords
+        tokens = [token for token in tokens if token not in lang_stopwords]
+    if stem:
+        # # stemming
+        lang = "english"
+        stemmer = SnowballStemmer(lang)
+        tokens = [stemmer.stem(token) for token in tokens]
+    if lem:
+        lemmatizer = WordNetLemmatizer()
+        tokens = [lemmatizer.lemmatize(token) for token in tokens]
+    return tokens
